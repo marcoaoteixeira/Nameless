c0f810792a6111bf3ad449f15e70b9974b009e09
marcoaoteixeira <marcoaoteixeira@gmail.com>
2023-07-20 17:15:04 -0300

    Added error output for validation filter and get a better output for API.

---
+++ src/AspNetCore/Nameless.AspNetCore/ErrorOutput.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Nameless.AspNetCore {
+
+    public sealed record Error {
+        #region Public Properties
+
+        [JsonPropertyName("code")]
+        public string Code { get; init; } = null!;
+
+        [JsonPropertyName("problems")]
+        public string[] Problems { get; init; } = null!;
+
+        #endregion
+    }
+
+    public sealed class ErrorOutput : IEnumerable<Error> {
+        #region Private Properties
+
+        private List<Error> Bag { get; } = new();
+
+        #endregion
+
+        #region Public Methods
+
+        public void Push(string code, string[] messages) {
+            Prevent.Against.Null(code, nameof(code));
+            Prevent.Against.Null(messages, nameof(messages));
+
+            Bag.Add(new() {
+                Code = code,
+                Problems = messages
+            });
+        }
+
+        public void Push(Exception ex) {
+            Prevent.Against.Null(ex, nameof(ex));
+
+            Bag.Add(new() {
+                Code = ex.Message,
+                Problems = ex.StackTrace?.Split(Environment.NewLine) ?? Array.Empty<string>()
+            });
+        }
+
+        #endregion
+
+        #region IEnumerable<Error> Members
+
+        IEnumerator<Error> IEnumerable<Error>.GetEnumerator()
+            => Bag.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => Bag.GetEnumerator();
+
+        #endregion
+    }
+
+    public static class ModelStateDictionaryExtension {
+        #region Public Static Methods
+
+        public static ErrorOutput CreateErrorOutput(this ModelStateDictionary self) {
+            var errorOutput = new ErrorOutput();
+
+            foreach (var kvp in self) {
+                var code = kvp.Key;
+                var messages = new List<string>();
+                foreach (var error in kvp.Value.Errors) {
+                    var message = string.IsNullOrWhiteSpace(error.ErrorMessage)
+                        ? error.Exception?.Message ?? string.Empty
+                        : string.Empty;
+
+                    messages.Add(message);
+                }
+                errorOutput.Push(code, messages.ToArray());
+            }
+
+            return errorOutput;
+        }
+
+        #endregion
+    }
+}
