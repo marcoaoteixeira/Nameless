bbdec8f1d67331145cd4d211a534d0c85398c13c
marcoaoteixeira <marcoaoteixeira@gmail.com>
2023-08-22 17:42:20 -0300

    Set ignorecase=false for git config

---
+++ src/MongoDB/Nameless.MongoDB.DependencyInjection/Bootstrapper.cs
@@ -0,0 +1,42 @@
+﻿using Autofac;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Nameless.MongoDB.DependencyInjection {
+    public sealed class Bootstrapper : IStartable {
+        #region Private Read-Only Fields
+
+        private readonly Type[] _mappings;
+
+        #endregion
+
+        #region Public Properties
+
+        private ILogger? _logger;
+        public ILogger Logger {
+            get => _logger ??= NullLogger.Instance;
+            set => _logger = value;
+        }
+
+        #endregion
+
+        #region Public Constructors
+
+        public Bootstrapper(Type[] mappings) {
+            _mappings = mappings ?? Array.Empty<Type>();
+        }
+
+        #endregion
+
+        #region IStartable Members
+
+        public void Start() {
+            foreach (var mapping in _mappings) {
+                try { Activator.CreateInstance(mapping); }
+                catch (Exception ex) { Logger.LogError(ex, "Error while running mapping {mapping}", mapping); }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/MongoDB/Nameless.MongoDB/IMongoCollectionProvider.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Driver;
+
+namespace Nameless.MongoDB {
+    public interface IMongoCollectionProvider {
+        #region Methods
+
+        IMongoCollection<T> GetCollection<T>(string? name = null, MongoCollectionSettings? settings = null);
+
+        #endregion
+    }
+}
