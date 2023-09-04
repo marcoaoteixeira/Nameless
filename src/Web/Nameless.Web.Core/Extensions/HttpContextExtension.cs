b3a4f501906d7f666647509b2a3c3a2122580d2b
marcoaoteixeira <marcoaoteixeira@gmail.com>
2023-09-04 10:54:24 -0300

    Added IHttpContextAccessor to HttpContextExtension

--- src/Web/Nameless.Web.Core/Extensions/HttpContextExtension.cs
+++ src/Web/Nameless.Web.Core/Extensions/HttpContextExtension.cs
@@ -13,21 +13,45 @@ namespace Nameless.Web {
         /// </summary>
         /// <param name="self">The current instance that implements <see cref="HttpContext"/>.</param>
         /// <returns>A <see cref="string"/> representation of the IPv4.</returns>
-        /// <remarks>If the IP address could not be parsed, a <see cref="string.Empty"/> will be returned instead.</remarks>
+        /// <remarks>If the IP address could not be parsed, a <see cref="IPAddress.None"/> will be returned instead.</remarks>
         public static string GetIPv4(this HttpContext self)
             => GetIPAddress(self).MapToIPv4().ToString();
+
+        /// <summary>
+        /// Retrieves the IP address (v4) from the <see cref="IHttpContextAccessor"/>.
+        /// </summary>
+        /// <param name="self">The current instance that implements <see cref="IHttpContextAccessor"/>.</param>
+        /// <returns>A <see cref="string"/> representation of the IPv4.</returns>
+        /// <remarks>If the IP address could not be parsed, a <see cref="IPAddress.None"/> will be returned instead.</remarks>
+        public static string GetIPv4(this IHttpContextAccessor self)
+            => GetIPAddress(self).MapToIPv4().ToString();
+
         /// <summary>
         /// Retrieves the IP address (v6) from the <see cref="HttpContext"/>.
         /// </summary>
         /// <param name="self">The current instance that implements <see cref="HttpContext"/>.</param>
         /// <returns>A <see cref="string"/> representation of the IPv6.</returns>
-        /// <remarks>If the IP address could not be parsed, a <see cref="string.Empty"/> will be returned instead.</remarks>
+        /// <remarks>If the IP address could not be parsed, a <see cref="IPAddress.None"/> will be returned instead.</remarks>
         public static string GetIPv6(this HttpContext self)
+            => GetIPAddress(self).MapToIPv6().ToString();
+
+        /// <summary>
+        /// Retrieves the IP address (v6) from the <see cref="IHttpContextAccessor"/>.
+        /// </summary>
+        /// <param name="self">The current instance that implements <see cref="IHttpContextAccessor"/>.</param>
+        /// <returns>A <see cref="string"/> representation of the IPv4.</returns>
+        /// <remarks>If the IP address could not be parsed, a <see cref="IPAddress.None"/> will be returned instead.</remarks>
+        public static string GetIPv6(this IHttpContextAccessor self)
             => GetIPAddress(self).MapToIPv6().ToString();
 
         #endregion
 
         #region Private Static Methods
+
+        private static IPAddress GetIPAddress(IHttpContextAccessor httpContextAccessor)
+            => httpContextAccessor.HttpContext is not null
+                ? GetIPAddress(httpContextAccessor.HttpContext)
+                : IPAddress.None;
 
         private static IPAddress GetIPAddress(HttpContext httpContext)
             => httpContext.Request.Headers.ContainsKey(Root.HttpRequestHeaders.X_FORWARDED_FOR)
