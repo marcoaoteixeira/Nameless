ebaebdce9e84d7505039d299194ce9acb2aa2ec5
Marco Teixeira <marcoaoteixeira@gmail.com>
2024-11-11 17:46:12 +0000

    fix: MongoDB unit test

--- tests/MongoDB/Nameless.MongoDB.UnitTest/DependencyInjectionTests.cs
+++ tests/MongoDB/Nameless.MongoDB.UnitTest/DependencyInjectionTests.cs
@@ -1,12 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using Nameless.MongoDB.Fixtures.Entities;
 using Nameless.MongoDB.Fixtures.Mappings;
 
 namespace Nameless.MongoDB;
 
 public class DependencyInjectionTests {
-    [Category(Categories.RUNS_ON_DEV_MACHINE)]
-    [Test(Description = "You'll need a local mongo instance or configure it to access a remote instance. See README file.")]
+    [Test]
     public void Register_Resolve_MongoDB_Services() {
         // arrange
         var services = new ServiceCollection();
@@ -20,14 +18,6 @@ public class DependencyInjectionTests {
         // act
         var service = provider.GetService<IMongoCollectionProvider>();
 
-        var collection = service.GetCollection<Animal>(name: string.Empty, settings: null);
-
-        collection.InsertOne(new Animal {
-            ID = Guid.NewGuid(),
-            Name = "Bolota",
-            Species = "Dog"
-        });
-        
         // assert
         Assert.That(service, Is.InstanceOf<MongoCollectionProvider>());
     }
