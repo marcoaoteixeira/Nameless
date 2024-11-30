1f9e947eb35346f3d95786436c06965e145dea0c
Marco Teixeira <marcoaoteixeira@gmail.com>
2024-11-30 21:04:36 +0000

    Caching project removed.

--- src/Caching/Nameless.Caching.Redis/IConnectionMultiplexerManager.cs
+++
@@ -1,7 +0,0 @@
-﻿using StackExchange.Redis;
-
-namespace Nameless.Caching.Redis;
-
-public interface IConnectionMultiplexerManager {
-    Task<IConnectionMultiplexer> GetConnectionMultiplexerAsync();
-}
