b675586bd78f108f2d47696fed89c16e93a3c115
Marco Teixeira <marcoaoteixeira@gmail.com>
2025-04-27 16:37:39 +0100

    Result convert to class instead struct, so it can be inherited

--- src/Core/Nameless.Core/Results/Result.T0.cs
+++ src/Core/Nameless.Core/Results/Result.T0.cs
@@ -1,35 +1,8 @@
 ﻿namespace Nameless.Results;
 
-public readonly struct Result<TArg0> : IResult {
-    private readonly TArg0? _arg0;
-    private readonly Error[]? _errors;
-
-    public object? Value
-        => Index switch {
-            0 => _arg0,
-            1 => _errors,
-            _ => throw new InvalidOperationException("Invalid index")
-        };
-    public int Index { get; }
-    public bool IsArg0 => Index == 0;
-    public bool HasErrors => Index == 1;
-    public TArg0? AsArg0 => IsArg0
-        ? _arg0
-        : throw new InvalidOperationException("Arg0 value not available");
-    public Error[] AsErrors => HasErrors
-        ? _errors ?? []
-        : throw new InvalidOperationException("Arg1 value not available");
-
-    public Result() {
-        throw new InvalidOperationException("Do not use type constructor.");
-    }
-
-    private Result(int index, TArg0? arg0 = default, Error[]? errors = null) {
-        Index = index;
-
-        _arg0 = arg0;
-        _errors = errors;
-    }
+public sealed class Result<TArg0> : ResultBase<TArg0> {
+    private Result(int index, TArg0? arg0 = default, Error[]? errors = null)
+        : base(index, arg0, errors) { }
 
     public static implicit operator Result<TArg0>(TArg0 arg0)
         => new(0, arg0: arg0);
@@ -39,35 +12,4 @@ public readonly struct Result<TArg0> : IResult {
 
     public static implicit operator Result<TArg0>(Error[] errors)
         => new(1, errors: errors);
-
-    public void Switch(Action<TArg0?> onArg0, Action<Error[]>? onErrors = null) {
-        if (IsArg0) {
-            Prevent.Argument.Null(onArg0)(AsArg0);
-            return;
-        }
-
-        if (HasErrors && onErrors is not null) {
-            onErrors(AsErrors);
-        }
-    }
-
-    public Task Switch(Func<TArg0?, Task> onArg0, Func<Error[], Task>? onErrors = null) {
-        if (IsArg0) {
-            return Prevent.Argument.Null(onArg0)(AsArg0);
-        }
-
-        return HasErrors && onErrors is not null
-            ? onErrors(AsErrors)
-            : Task.CompletedTask;
-    }
-
-    public TResult Match<TResult>(Func<TArg0?, TResult> onArg0, Func<Error[], TResult>? onErrors = null) {
-        if (IsArg0) {
-            return Prevent.Argument.Null(onArg0)(AsArg0);
-        }
-
-        return HasErrors && onErrors is not null
-            ? onErrors(AsErrors)
-            : default!;
-    }
 }
---
+++ src/Core/Nameless.Core/Results/ResultBase.T2.cs
@@ -0,0 +1,104 @@
+﻿namespace Nameless.Results;
+
+public abstract class ResultBase<TArg0, TArg1, TArg2> : IResult {
+    private readonly TArg0? _arg0;
+    private readonly TArg1? _arg1;
+    private readonly TArg2? _arg2;
+    private readonly Error[]? _errors;
+
+    public object? Value
+        => Index switch {
+            0 => _arg0,
+            1 => _arg1,
+            2 => _arg2,
+            3 => _errors,
+            _ => throw new InvalidOperationException("Invalid index")
+        };
+    public int Index { get; }
+    public bool IsArg0 => Index == 0;
+    public bool IsArg1 => Index == 1;
+    public bool IsArg2 => Index == 2;
+    public bool HasErrors => Index == 3;
+    public TArg0? AsArg0 => IsArg0
+        ? _arg0
+        : throw new InvalidOperationException("Arg0 value not available");
+    public TArg1? AsArg1 => IsArg1
+        ? _arg1
+        : throw new InvalidOperationException("Arg1 value not available");
+    public TArg2? AsArg2 => IsArg2
+        ? _arg2
+        : throw new InvalidOperationException("Arg1 value not available");
+    public Error[] AsErrors => HasErrors
+        ? _errors ?? []
+        : throw new InvalidOperationException("Arg1 value not available");
+
+    protected ResultBase() {
+        throw new InvalidOperationException("Do not use type parameterless constructor.");
+    }
+
+    protected ResultBase(int index, TArg0? arg0 = default, TArg1? arg1 = default, TArg2? arg2 = default, Error[]? errors = null) {
+        Index = index;
+
+        _arg0 = arg0;
+        _arg1 = arg1;
+        _arg2 = arg2;
+        _errors = errors;
+    }
+
+    public void Switch(Action<TArg0?> onArg0, Action<TArg1?> onArg1, Action<TArg2?> onArg2, Action<Error[]>? onErrors = null) {
+        if (IsArg0) {
+            Prevent.Argument.Null(onArg0)(AsArg0);
+            return;
+        }
+
+        if (IsArg1) {
+            Prevent.Argument.Null(onArg1)(AsArg1);
+            return;
+        }
+
+        if (IsArg2) {
+            Prevent.Argument.Null(onArg2)(AsArg2);
+            return;
+        }
+
+        if (HasErrors && onErrors is not null) {
+            onErrors(AsErrors);
+        }
+    }
+
+    public Task Switch(Func<TArg0?, Task> onArg0, Func<TArg1?, Task> onArg1, Func<TArg2?, Task> onArg2, Func<Error[], Task>? onErrors = null) {
+        if (IsArg0) {
+            return Prevent.Argument.Null(onArg0)(AsArg0);
+        }
+
+        if (IsArg1) {
+            return Prevent.Argument.Null(onArg1)(AsArg1);
+        }
+
+        if (IsArg2) {
+            return Prevent.Argument.Null(onArg2)(AsArg2);
+        }
+
+        return HasErrors && onErrors is not null
+            ? onErrors(AsErrors)
+            : Task.CompletedTask;
+    }
+
+    public TResult Match<TResult>(Func<TArg0?, TResult> onArg0, Func<TArg1?, TResult> onArg1, Func<TArg2?, TResult> onArg2, Func<Error[], TResult>? onErrors = null) {
+        if (IsArg0) {
+            return Prevent.Argument.Null(onArg0)(AsArg0);
+        }
+
+        if (IsArg1) {
+            return Prevent.Argument.Null(onArg1)(AsArg1);
+        }
+
+        if (IsArg2) {
+            return Prevent.Argument.Null(onArg2)(AsArg2);
+        }
+
+        return HasErrors && onErrors is not null
+            ? onErrors(AsErrors)
+            : default!;
+    }
+}
